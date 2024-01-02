770d3d51cb809abb6bc8237db41f6b547e78d04e
jun <kimmj@moadata.co.kr>
2024-01-02 16:54:34 +0900

    세션 7 : 고급 매핑 @Inheritance(strategy = InheritanceType.JOINED) @MappedSuperclass 를 이용한 dt관련 관리.

--- src/main/java/jpabook/jpashop/domain/BaseEntity.java
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -5,7 +5,7 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     private String createBy;
     private LocalDateTime createDate;
